86aee4bab2fccf05a1f9cf071331e51f585c3522
lijunjun <11776174@qq.com>
2023-01-15 18:25:53 +0800

    feat: 增加入职时间字段

--- src/com/imooc/jdbc/hrapp/command/InsertCommand.java
+++ src/com/imooc/jdbc/hrapp/command/InsertCommand.java
@@ -3,8 +3,11 @@ package com.imooc.jdbc.hrapp.command;
 import com.imooc.jdbc.hrapp.common.DBUtils;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 public class InsertCommand implements Command {
@@ -19,14 +22,29 @@ public class InsertCommand implements Command {
         float salary = scan.nextFloat();
         System.out.println("请输入员工部门");
         String dName = scan.next();
+        System.out.println("请输入入职日期");
+        // String => java.sql.Date步骤
+        // 1. String 转为 java.util.Date
+        // 2. 将 java.util.Date 转换为时间戳
+        // 3. java.sql.Date 支持传入时间戳
+        String strDate = scan.next();
+        java.util.Date udDate = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            udDate = sdf.parse(strDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Date sqDate = new Date(udDate.getTime());
         Connection conn = null;
         try {
             conn = DBUtils.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement("insert into imooc.employee(eno, ename, salary, dname) values (?,?,?,?)");
+            PreparedStatement pstmt = conn.prepareStatement("insert into imooc.employee(eno, ename, salary, dname, hiredate) values (?,?,?,?,?)");
             pstmt.setInt(1, eno);
             pstmt.setString(2, eName);
             pstmt.setFloat(3, salary);
             pstmt.setString(4, dName);
+            pstmt.setDate(5, sqDate);
             int count = pstmt.executeUpdate();
             System.out.println("count: " + count);
             System.out.println(eName + "的入职手续已办理");
